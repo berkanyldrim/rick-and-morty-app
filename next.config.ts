dbb51ca128b3e378faade2be639151b2c1fb220c
berkanyldrim <berkanyldrim@gmail.com>
2025-02-25 22:56:11 +0300

    feat: first commit

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rickandmortyapi.com",
+        pathname: "/api/character/avatar/**",
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
